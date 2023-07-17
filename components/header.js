48f763d2b29078851eae5b8df8d25dcd541a4781
Nicolas Davister <nidavister@outlook.com>
2023-07-17 15:33:08 +0200

    Created header and footer for the website
    header and footer are defined as components, written in javascript in the subfolder "components". They can be reuse through several html pages and written once.

---
+++ components/header.js
@@ -0,0 +1,132 @@
+
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<style>
+    .navbar {
+	  overflow: hidden;
+	  background-color: #333;
+	  font-family: "Times New Roman", Times, serif;
+	}
+
+	.navbar a {
+	  float: left;
+	  font-size: 16px;
+	  color: white;
+	  text-align: center;
+	  padding: 14px 80px;
+	  text-decoration: none;
+	}
+
+
+	.subnav {
+	  float: left;
+	  overflow: hidden;
+	}
+
+	.subnav .subnavbtn {
+	  font-size: 16px;  
+	  border: none;
+	  outline: none;
+	  color: white;
+	  padding: 14px 80px;
+	  background-color: inherit;
+	  font-family: inherit;
+	  margin: 0;
+	}
+	.navbar {
+	  overflow: hidden;
+	  background-color: #333;
+	  font-family: "Times New Roman", Times, serif;
+	}
+
+	.navbar a {
+	  float: left;
+	  font-size: 16px;
+	  color: white;
+	  text-align: center;
+	  padding: 14px 80px;
+	  text-decoration: none;
+	}
+
+
+	.subnav {
+	  float: left;
+	  overflow: hidden;
+	}
+
+	.subnav .subnavbtn {
+	  font-size: 16px;  
+	  border: none;
+	  outline: none;
+	  color: white;
+	  padding: 14px 80px;
+	  background-color: inherit;
+	  font-family: inherit;
+	  margin: 0;
+	}
+
+	.navbar a:hover, .subnav:hover .subnavbtn {
+	  background-color: black;
+	}
+
+	.subnav-content {
+	  display: none;
+	  position: absolute;
+	  left: 0;
+	  background-color: black;
+	  width: 100%;
+	  z-index: 1;
+	}
+
+	.subnav-content a {
+	  float: left;
+	  color: white;
+	  text-decoration: none;
+	}
+
+	.subnav-content a:hover {
+	  background-color: #333;
+	  color: white;
+	}
+
+	.subnav:hover .subnav-content {
+	  display: block;
+	}
+  </style>
+  <header>
+    <div class="navbar">
+      <a href="./index.html">Home</a>
+      <div class="subnav">
+        <button class="subnavbtn">Gallery <i class="fa fa-caret-down"></i></button>
+        <div class="subnav-content">
+          <a href="#pictures">Pictures</a>
+          <a href="./videos_25_June_2023.html">Videos</a>
+        </div>
+      </div> 
+      <div class="subnav">
+        <button class="subnavbtn">Simulation <i class="fa fa-caret-down"></i></button>
+        <div class="subnav-content">
+          <a href="#mathModel">Math Model</a>
+          <a href="#trebuchetMotion">Trebuchet Motion</a>
+        </div>
+      </div> 
+      <a href="#contact">Contact</a>
+    </div>
+  </header>
+`;
+
+
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+  	const shadowRoot = this.attachShadow({ mode: 'open' });
+  	shadowRoot.appendChild(headerTemplate.content);
+  }
+}
+
+customElements.define('header-component', Header);
